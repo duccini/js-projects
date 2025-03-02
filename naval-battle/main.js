d011291110964afed3b32a76910155565147e914
duccini <guilhermeduccini@gmail.com>
2025-03-02 11:39:13 -0300

    create flip ships function

--- naval-battle/main.js
+++ naval-battle/main.js
@@ -1,1 +1,15 @@
-console.log("ola mundo");
+const flipButton = document.querySelector("#flip-button");
+const optionContainer = document.querySelector(".option-container");
+
+let angle = 0;
+
+function flip() {
+  // children => HTML Collection
+  const optionShips = Array.from(optionContainer.children);
+
+  angle = angle === 0 ? 90 : 0;
+
+  optionShips.forEach((ship) => (ship.style.transform = `rotate(${angle}deg)`));
+}
+
+flipButton.addEventListener("click", flip);

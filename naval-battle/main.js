6fa96e4b2fe3448b1ec9be2cc5e7f28ff67e7846
duccini <guilhermeduccini@gmail.com>
2025-03-02 18:14:14 -0300

    create add computer ships

--- naval-battle/main.js
+++ naval-battle/main.js
@@ -14,6 +14,8 @@ function flip() {
   optionShips.forEach((ship) => (ship.style.transform = `rotate(${angle}deg)`));
 }
 
+flipButton.addEventListener("click", flip);
+
 // Creating Boards
 const width = 10;
 
@@ -37,4 +39,44 @@ function createBoard(color, user) {
 createBoard("SkyBlue", "player");
 createBoard("DodgerBlue", "computer");
 
-flipButton.addEventListener("click", flip);
+// Create Ships
+class Ship {
+  constructor(name, length) {
+    this.name = name;
+    this.length = length;
+  }
+}
+
+const destroyer = new Ship("destroyer", 2);
+const submarine = new Ship("submarine", 3);
+const cruiser = new Ship("cruiser", 3);
+const blattleship = new Ship("blattleship", 4);
+const carrier = new Ship("carrier", 5);
+
+const ships = [destroyer, submarine, cruiser, blattleship, carrier];
+
+function addComputerShips(ship) {
+  const allBoardBlocks = document.querySelectorAll("#computer div");
+
+  // Random Boolean
+  let isHorizontal = Math.random() < 0.5;
+
+  let randomStartIndex = Math.floor(Math.random() * width * width);
+
+  let shipBlocks = [];
+
+  for (let i = 0; i < ship.length; i++) {
+    if (isHorizontal) {
+      shipBlocks.push(allBoardBlocks[Number(randomStartIndex) + i]);
+    } else {
+      shipBlocks.push(allBoardBlocks[Number(randomStartIndex) + i * width]);
+    }
+  }
+
+  shipBlocks.forEach((shipBlock) => {
+    shipBlock.classList.add(ship.name);
+    shipBlock.classList.add("taken");
+  });
+}
+
+ships.forEach((ship) => addComputerShips(ship));

fcfce306d39562a34c4a942d72148b31be8aaa2b
duccini <guilhermeduccini@gmail.com>
2025-03-02 16:38:04 -0300

    create game board

--- naval-battle/main.js
+++ naval-battle/main.js
@@ -1,6 +1,8 @@
 const flipButton = document.querySelector("#flip-button");
 const optionContainer = document.querySelector(".option-container");
+const gamesboardContainer = document.querySelector("#gamesboard-container");
 
+// Option choosing
 let angle = 0;
 
 function flip() {
@@ -11,5 +13,28 @@ function flip() {
 
   optionShips.forEach((ship) => (ship.style.transform = `rotate(${angle}deg)`));
 }
+
+// Creating Boards
+const width = 10;
+
+function createBoard(color, user) {
+  const gameBoardContainer = document.createElement("div");
+  gameBoardContainer.classList.add("game-board");
+  gameBoardContainer.style.backgroundColor = color;
+  gameBoardContainer.id = user;
+
+  for (let i = 0; i < width * width; i++) {
+    const block = document.createElement("div");
+    block.classList.add("block");
+    block.id = i;
+
+    gameBoardContainer.append(block);
+  }
+
+  gamesboardContainer.append(gameBoardContainer);
+}
+
+createBoard("SkyBlue", "player");
+createBoard("DodgerBlue", "computer");
 
 flipButton.addEventListener("click", flip);
